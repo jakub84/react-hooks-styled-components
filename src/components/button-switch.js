410e353cb9f2172aaf6dd35f02d83f4641ce02e3
Jakub <jakub.d84@gmail.com>
2019-05-23 19:03:05 +0200

    Setup app

---
+++ src/components/button-switch.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const ButtonSwicher = () => {
+    return (
+        <div>
+            <p>Select a maintenance drone:</p>
+
+            <div>
+                <input type="radio" id="huey" name="drone" value="huey"
+                    checked />
+                    <label for="huey">Huey</label>
+</div>
+
+                    </div>
+                    );
+                  }
+                  
+export default ButtonSwicher;
